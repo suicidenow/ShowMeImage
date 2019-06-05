dfab1648d8cb898f3ee240bae927211e8961e4fe
suicidenow <35851331+suicidenow@users.noreply.github.com>
2019-06-05 17:22:34 +0500

    Update smi.js

--- smi.js
+++ smi.js
@@ -1,1 +1,19 @@
-$.fn.ShowMeImage=function(n){$(this).on("change",function(){var e=$(this)[0].files;$.each(e,function(e,i){var t=this.name,a=new FileReader;a.readAsDataURL(this),a.onloadend=function(){$(""+n).append('<div class="attach-file" data-file="'+t+'"><img src="'+a.result+'"><input type="file" style="display:none;" value="'+a.result+'"><div class="attach-file-delete">&times;</div></div>'),$(".attach-file").each(function(e){$(this).find("input").attr("name","file-"+e)}),$(".attach-file").on("click",function(){$(this).remove()})}})})};
+$.fn.ShowMeImage = function (param) {
+    $(this).on("change", function() {
+        var files = $(this)[0].files;
+        $.each(files, function(i, t) {
+            var fileName = this.name
+              , newFile = new FileReader;
+            newFile.readAsDataURL(this),
+            newFile.onloadend = function() {
+                $(""+param+"").append('<div class="attach-file" data-file="' + fileName + '"><img src="' + newFile.result + '"><input type="file" style="display:none;" value="' + newFile.result + '"><div class="attach-file-delete">&times;</div></div>'),
+                $(".attach-file").each(function(i) {
+                    $(this).find("input").attr("name", "file-" + i)
+                }),
+                $(".attach-file").on("click", function() {
+                    $(this).remove()
+                })
+            }
+        })
+    });
+};
